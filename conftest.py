ed9f81acc3a1961fc88ad8a861d761c20d4b9db5
avrelin-aleksey <142740933+avrelin-aleksey@users.noreply.github.com>
2025-04-26 16:30:02 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
 import allure
 import pytest
+import os
 
+from selenium import webdriver
 from selenium import webdriver
 from pages.collections import CollectionPage
 from pages.customer import CustomerPage
