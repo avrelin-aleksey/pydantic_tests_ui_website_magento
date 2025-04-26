8f21dabb39628aae5b135b4f303f8d53112e5c14
avrelin-aleksey <142740933+avrelin-aleksey@users.noreply.github.com>
2025-04-26 16:35:11 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,42 +1,39 @@
-import allure
-import pytest
 import os
-
+import pytest
 from selenium import webdriver
-from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from pages.collections import CollectionPage
 from pages.customer import CustomerPage
 from pages.sale import SalePage
-from selenium.webdriver.chrome.options import Options
-
-
 
 @pytest.fixture()
 def driver():
     options = Options()
     options.page_load_strategy = "eager"
-
+    
+    # Настройки для CI 
     if os.getenv('CI') == 'true':
-        options.add_argument("--headless")  # Без графического интерфейса
-        options.add_argument("--no-sandbox")  # Необходимо для Linux
-        options.add_argument("--disable-dev-shm-usage")  # Для ограниченных ресурсов
-        options.add_argument("--disable-gpu")  # Отключение GPU в CI
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
         service = Service(executable_path='/usr/bin/chromedriver')
-    else:    
+    else:
+        # Настройки для локального запуска
         options.add_experimental_option("detach", True)
         service = Service()
-        
-    chrome_driver = webdriver.Chrome(options=options)
-    chrome_driver.maximize_window()
     
-    yield chrome_driver
-    chrome_driver.quit()
-
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.maximize_window()
+    
+    yield driver
+    chrome_driver.save_screenshot(f'screen{random.randrange(1, 10000)}.png') 
+    driver.quit()
 
 @pytest.fixture()
 def customer_page(driver):
     return CustomerPage(driver)
-
 
 @pytest.fixture()
 def collection_page(driver):
@@ -45,5 +42,3 @@ def collection_page(driver):
 @pytest.fixture()
 def sale_page(driver):
     return SalePage(driver)
-
-
