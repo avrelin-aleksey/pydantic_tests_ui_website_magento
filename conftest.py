b8bfa96848a8eb2d7bdc92f241f9f8743b7cd2d4
avrelin-aleksey <142740933+avrelin-aleksey@users.noreply.github.com>
2025-04-26 17:00:19 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,30 +1,33 @@
-import os
-import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+import allure
+import pytest
+import random
+
+from selenium import webdriver
 from pages.collections import CollectionPage
 from pages.customer import CustomerPage
 from pages.sale import SalePage
+from selenium.webdriver.chrome.options import Options
+
+
 
 @pytest.fixture()
 def driver():
     options = Options()
+    options.add_experimental_option("detach", True)
     options.page_load_strategy = "eager"
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
-    
-    yield driver
-    chrome_driver.save_screenshot(f'screen{random.randrange(1, 10000)}.png') 
-    driver.quit()
+    chrome_driver = webdriver.Chrome(options=options)
+    chrome_driver.maximize_window()
+    yield chrome_driver
+    if request.node.rep_call.failed:
+        chrome_driver.save_screenshot(f'screen_fail_{random.randrange(1, 10000)}.png')
+    chrome_driver.quit()
+
 
 @pytest.fixture()
 def customer_page(driver):
     return CustomerPage(driver)
+
 
 @pytest.fixture()
 def collection_page(driver):
@@ -33,3 +36,4 @@ def collection_page(driver):
 @pytest.fixture()
 def sale_page(driver):
     return SalePage(driver)
+
